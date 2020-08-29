5cb01404cf5d0037eecbf5e91c9e779ec5c6e48e
nithyajabil <70336971+nithyajabil@users.noreply.github.com>
2020-08-30 00:23:15 +0800

    Add files via upload

---
+++ myapi/urls.py
@@ -0,0 +1,10 @@
+# myapi/urls.py
+from django.urls import include, path
+from rest_framework import routers
+from . import views
+
+router = routers.DefaultRouter()
+router.register(r'heroes', views.HeroViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
